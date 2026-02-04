c9f8166b2b2483ee60a76d99b02f6f6da32aece0
astheressix31416@gmail.com <dpaless0409@gmail.com>
2026-02-03 23:04:44 -0500

    Actualizando dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -16,6 +17,7 @@ import { PrestamoModule } from './prestamo/prestamo.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     PrismaModule,
     FacultadModule,
